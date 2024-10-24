2ad0204b0f4c692df9a744895c9d34b10faef279
qasiagul <qasiagul633@gmail.com>
2024-10-24 15:19:11 +0500

    add authenticaion

--- lib/customs/product_card.dart
+++ lib/customs/product_card.dart
@@ -1,46 +1,46 @@
-import 'package:flutter/material.dart';
-import 'package:fruite_hub_ecommerce_app/customs/custom_card.dart';
-import 'package:fruite_hub_ecommerce_app/data/data.dart';
+// import 'package:flutter/material.dart';
+// import 'package:fruite_hub_ecommerce_app/customs/custom_card.dart';
+// import 'package:fruite_hub_ecommerce_app/data/data.dart';
 
-class ProductCard extends StatefulWidget {
-  const ProductCard({super.key});
+// class ProductCard extends StatefulWidget {
+//   const ProductCard({super.key});
 
-  @override
-  State<ProductCard> createState() => _ProductCardState();
-}
+//   @override
+//   State<ProductCard> createState() => _ProductCardState();
+// }
 
-class _ProductCardState extends State<ProductCard> {
+// class _ProductCardState extends State<ProductCard> {
   
-  List<Product> _productList = productList;
+//   List<Product> _productList = productList;
 
-  List<Product> _filteredList = [];
+//   List<Product> _filteredList = [];
 
 
 
-  updateFilteredList(String category) {
-    _filteredList =
-        productList.where((product) => product.category == category).toList();
-    setState(() {});
-  }    
-  @override
-  Widget build(BuildContext context) {
+//   updateFilteredList(String category) {
+//     _filteredList =
+//         productList.where((product) => product.category == category).toList();
+//     setState(() {});
+//   }    
+//   @override
+//   Widget build(BuildContext context) {
 
-    return Scaffold(
-      body: Column(
-        children: [
-          SingleChildScrollView(
-                      scrollDirection: Axis.horizontal,
-                      child: Row(
-          children: 
-            productList.map((e){
-              return FruitComboCard(title: e.title, price: e.price.toString(), image: e.imageUrl, color:e.color, route:e.route);
-            }).toList()
+//     return Scaffold(
+//       body: Column(
+//         children: [
+//           SingleChildScrollView(
+//                       scrollDirection: Axis.horizontal,
+//                       child: Row(
+//           children: 
+//             productList.map((e){
+//               return FruitComboCard(title: e.title, price: e.price.toString(), image: e.imageUrl, color:e.color, route:e.route);
+//             }).toList()
             
-                      ),
-                    ),
+//                       ),
+//                     ),
 
-        ],
-      ),
-    );
-  }
-}
+//         ],
+//       ),
+//     );
+//   }
+// }
